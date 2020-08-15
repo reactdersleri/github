e88ee6e56ed69f1326384ed1ac08a1e8a0213081
unknown <aynurkozan22@gmail.com>
2020-08-15 03:22:07 +0300

    index.js olusturuldu

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-// Selam
-// Yeni branch gecildi
+import React, {useState} from 'react';
+
+function Deneme () {
+
+    const [count, setCount] = useState (0);
+
+    const handleClickEkle = (quantity) => {
+        setCount (count + quantity);
+    };
+
+    const handleClickCikar = (quantity) => {
+        setCount (count - quantity);
+    };
+
+    return (
+        <div>
+            HomePage
+            <br />
+            {count}
+            <button onClick={()=> handleClickEkle (1)}>ekle</button>
+            <button onClick = { () => handleClickCikar (1)}>çıkar</button>
+
+
+
+        </div>
+    );
+
+}
+
+export default Deneme;
+
+
